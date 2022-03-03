94fa1d263ed59e8560b5bfa47d99776611aaab41
KarmilaNoviArfiana <90132730+KarmilaNoviArfiana@users.noreply.github.com>
2022-03-03 12:25:20 +0700

    praktikum1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    return "<hr> Nim   : 2041720073 </hr> <br>
+            <hr> Nama  : Karmila Novi Arfiana </hr> <br>
+            <hr> Kelas : TI-2C </hr>";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel dengan ID " .$id;
 });
